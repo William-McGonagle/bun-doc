5f72a39d000eb33c6035a7d8e1cb24beac6f7b80
William McGonagle <wpmcgonagle@gmail.com>
2022-07-24 07:05:52 -0400

    Delete docs directory

--- docs/_next/static/94-RLunFOTC9BsiQHUMcN/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-9986da30fe5e6d9d.js"],"/_error":["static/chunks/pages/_error-942918685354aae9.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
