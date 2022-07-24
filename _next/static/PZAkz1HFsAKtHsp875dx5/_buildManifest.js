b704f51745df9ebd5518d461c709e46086b1daf1
William-McGonagle <wpmcgonagle@gmail.com>
2022-07-24 13:48:32 +0000

    Deploying to gh-pages from @ William-McGonagle/bun-doc@4690778411dcfeae0c16da105f2b1e155bbd5fb0 🚀

---
+++ _next/static/PZAkz1HFsAKtHsp875dx5/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-378910fc9177fbd8.js"],"/_error":["static/chunks/pages/_error-942918685354aae9.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
